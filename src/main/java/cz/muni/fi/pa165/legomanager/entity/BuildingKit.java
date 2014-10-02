564afe2d976e21c7ce3c36988ed93344edf32804
Petr <Petr@Petr-PC>
2014-10-02 16:20:12 +0200

    Test

--- src/main/java/cz/muni/fi/pa165/legomanager/entity/BuildingKit.java
+++ src/main/java/cz/muni/fi/pa165/legomanager/entity/BuildingKit.java
@@ -11,6 +11,15 @@ package cz.muni.fi.pa165.legomanager.entity;
  */
 public class BuildingKit {
     
+    
     private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
     
 }
