9ee0838ecd99398eadfa96aba8172c8955528582
lastovicka <lastovickam@gmail.com>
2014-10-31 09:23:25 +0100

    Added CategoryService interface and category transfer object

--- LegoManager-api/src/main/java/cz/muni/fi/pa165/legomanager/services/CategoryService.java
+++ LegoManager-api/src/main/java/cz/muni/fi/pa165/legomanager/services/CategoryService.java
@@ -5,10 +5,45 @@
  */
 package cz.muni.fi.pa165.legomanager.services;
 
+import cz.muni.fi.pa165.legomanager.transferobjects.CategoryTO;
+import cz.muni.fi.pa165.legomanager.transferobjects.LegoKitTO;
+import java.util.List;
+
 /**
  *
- * @author Tomas
+ * @author Martin Laštovička
  */
 public interface CategoryService {
+        
+     /**
+     * Finds all categories
+     * @return list of all categories
+     */
+    public List<LegoKitTO> getAllCategories() ;
     
+    /**
+     * Update category
+     * @param category
+     */
+    public void updateCategory(CategoryTO category);
+    
+    /**
+     * Removes category
+     * @param category to be deleted
+     * @throws IllegalArgumentException
+     */     
+    public void deleteCategory (CategoryTO category);
+    
+    /**
+     * Add new category
+     * @param category to be added   
+     */
+    public void createCategory (CategoryTO category);
+    
+    /**
+     * Find category by id
+     * @param id of wanted category
+     * @return category with specified id
+     */
+    public LegoKitTO getCategory (Long id);
 }
--- LegoManager-api/src/main/java/cz/muni/fi/pa165/legomanager/transferobjects/CategoryTO.java
+++ LegoManager-api/src/main/java/cz/muni/fi/pa165/legomanager/transferobjects/CategoryTO.java
@@ -5,10 +5,86 @@
  */
 package cz.muni.fi.pa165.legomanager.transferobjects;
 
+import java.util.List;
+import java.util.Objects;
+
 /**
  *
- * @author Tomas
+ * @author Martin Laštovička
  */
 public class CategoryTO {
+    private Long id;
+    private String name;
+    private String description;
+    private List<LegoKitTO> legoKits;
+    private List<LegoSetTO> legoSets;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<LegoKitTO> getLegoKits() {
+        return legoKits;
+    }
+
+    public void setLegoKits(List<LegoKitTO> legoKits) {
+        this.legoKits = legoKits;
+    }
+
+    public List<LegoSetTO> getLegoSets() {
+        return legoSets;
+    }
+
+    public void setLegoSets(List<LegoSetTO> legoSets) {
+        this.legoSets = legoSets;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 29 * hash + Objects.hashCode(this.id);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final CategoryTO other = (CategoryTO) obj;
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "CategoryTO{" + "id=" + id + ", name=" + name + ", description=" + description + ", legoKits=" + legoKits + ", legoSets=" + legoSets + '}';
+    }
+    
     
 }
