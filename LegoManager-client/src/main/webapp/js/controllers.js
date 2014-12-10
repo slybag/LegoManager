08c8289322e9db924f7983c70d02dafe282b6ef5
xhruso <xhruso@nymfe76.fi.muni.cz>
2014-12-10 16:48:51 +0100

    Legokit angular CRUD complete @Tomas

--- LegoManager-client/src/main/webapp/js/controllers.js
+++ LegoManager-client/src/main/webapp/js/controllers.js
@@ -1,9 +1,7 @@
 angular.module('legoApp.controllers', []).controller('LegoKitListController', function($scope, $state, LegoKit) {
     $scope.legoKits = LegoKit.query(); //fetch all lego kits. Issues a GET to /api/legoKits
-})//;
-//
-//angular.module('legoApp.controllers')
-        .controller('LegoKitCreateController', function($scope, $state, LegoKit, LegoPiece) {
+})
+.controller('LegoKitCreateController', function($scope, $state, LegoKit, LegoPiece) {
     $scope.legoKit = new LegoKit();
     LegoPiece.query(function(data){
         $scope.legoPieces = data;
@@ -20,9 +18,19 @@ angular.module('legoApp.controllers', []).controller('LegoKitListController', fu
             $state.go('legoKits');
         });
     };
-})//;
-//
-//angular.module('legoApp.controllers')
+})
+.controller('LegoKitReadController',function($scope, $state, $stateParams, LegoKit, LegoPiece){
+    LegoPiece.query(function(data){
+        $scope.legoPieces = data;
+    });
+
+    $scope.legoKit = LegoKit.get({ id: $stateParams.id },function(){ 
+        for(i=0; i<$scope.legoKit.legoPieces.length;i++){
+            $scope.selectedIds.push($scope.legoKit.legoPieces[i].id);
+        };
+    });
+    $scope.selectedIds = [];
+})
 .controller('LegoKitEditController',function($scope, $state, $stateParams, LegoKit, LegoPiece){
     LegoPiece.query(function(data){
         $scope.legoPieces = data;
@@ -46,55 +54,62 @@ angular.module('legoApp.controllers', []).controller('LegoKitListController', fu
             $state.go('legoKits');
         });
     };
-})//;
-//
-//angular.module('legoApp.controllers')
+})
+.controller('LegoKitDeleteController',function($scope, $state, $stateParams, LegoKit){
+    $scope.legoKit = LegoKit.get({ id: $stateParams.id });
+    $scope.deleteLegoKit = function(){
+        console.log($scope.legoKit);
+        $scope.legoKit.$delete(function(){
+            $state.go('legoKits');
+        });
+    };
+})
 .controller('LegoSetListController', function ($scope, $state, LegoSet) {
             $scope.legoSets = LegoSet.query(); //fetch all lego sets. Issues a GET to /api/legoSets
-        })
-        .controller('LegoPieceListController', function ($scope, $state, LegoPiece) {
+})
+.controller('LegoPieceListController', function ($scope, $state, LegoPiece) {
             $scope.legoPiece = LegoPiece.query(); //fetch all lego pieces. Issues a GET to /api/legoPieces
-        })
-        .controller('CategoryListController', function ($scope, $state, Category) {
+})
+.controller('CategoryListController', function ($scope, $state, Category) {
             $scope.categories = Category.query(); //fetch all lego categories. Issues a GET to /api/categories
-        })
-        .controller('CategoryCreateController', function ($scope, $state, Category, LegoKit, LegoSet) {
-            $scope.category = new Category();
-            $scope.legoKits = LegoKit.query();
-            $scope.legoSets = LegoSet.query();
-            $scope.legoKitSelected = [];
-            $scope.legoSetSelected = [];
-            $scope.addCategory = function () {
-                $scope.category.legoKits = $scope.legoKitSelected;
-                $scope.category.legoSets = $scope.legoSetSelected;
-                $scope.category.$save(function () {
-                    $state.go('categories');
-                });
-            };
-            $scope.toggleKitSelection = function toggleKitSelection(kit) {
-                var idx = $scope.legoKitSelected.indexOf(kit);
-
-                // is currently selected
-                if (idx > -1) {
-                    $scope.legoKitSelected.splice(idx, 1);
-                }
-
-                // is newly selected
-                else {
-                    $scope.legoKitSelected.push(kit);
-                }
-            };
-            $scope.toggleSetSelection = function toggleSetSelection(set) {
-                var idx = $scope.legoSetSelected.indexOf(set);
-
-                // is currently selected
-                if (idx > -1) {
-                    $scope.legoSetSelected.splice(idx, 1);
-                }
-
-                // is newly selected
-                else {
-                    $scope.legoSetSelected.push(set);
-                }
-            };
+})
+.controller('CategoryCreateController', function ($scope, $state, Category, LegoKit, LegoSet) {
+    $scope.category = new Category();
+    $scope.legoKits = LegoKit.query();
+    $scope.legoSets = LegoSet.query();
+    $scope.legoKitSelected = [];
+    $scope.legoSetSelected = [];
+    $scope.addCategory = function () {
+        $scope.category.legoKits = $scope.legoKitSelected;
+        $scope.category.legoSets = $scope.legoSetSelected;
+        $scope.category.$save(function () {
+            $state.go('categories');
         });
+    };
+    $scope.toggleKitSelection = function toggleKitSelection(kit) {
+        var idx = $scope.legoKitSelected.indexOf(kit);
+
+        // is currently selected
+        if (idx > -1) {
+            $scope.legoKitSelected.splice(idx, 1);
+        }
+
+        // is newly selected
+        else {
+            $scope.legoKitSelected.push(kit);
+        }
+    };
+    $scope.toggleSetSelection = function toggleSetSelection(set) {
+        var idx = $scope.legoSetSelected.indexOf(set);
+
+        // is currently selected
+        if (idx > -1) {
+            $scope.legoSetSelected.splice(idx, 1);
+        }
+
+        // is newly selected
+        else {
+            $scope.legoSetSelected.push(set);
+        }
+    };
+});
--- LegoManager-client/src/main/webapp/js/legoapp.js
+++ LegoManager-client/src/main/webapp/js/legoapp.js
@@ -3,26 +3,34 @@ angular.module('legoApp', ['ngResource','ui.router','legoApp.services','legoApp.
 
 angular.module('legoApp').config(function($stateProvider) {
     $stateProvider.state('legoKits', { // state for showing all kits
-        url: '/legoKits',
+        url: '/legoKits', // state for showing all kits
         templateUrl: 'legoKits.html',
         controller: 'LegoKitListController'
     }).state('newLegoKit', {
         url: '/legoKits/new',
         templateUrl: 'legokit_add.html',
         controller: 'LegoKitCreateController'
+    }).state('readLegoKit', {
+        url: '/legoKits/:id/read',
+        templateUrl: 'legokit_read.html',
+        controller: 'LegoKitReadController'
     }).state('updateLegoKit',{
         url: '/legoKits/:id/edit',
         templateUrl: 'legokit_edit.html',
         controller: 'LegoKitEditController'
+    }).state('deleteLegoKit',{
+        url: '/legoKits/:id/delete',
+        templateUrl: 'legokit_delete.html',
+        controller: 'LegoKitDeleteController'
     }).state('legoSets', { // state for showing all sets
         url: '/legoSets',
         templateUrl: 'legoSets.html',
         controller: 'LegoSetListController'
-    }).state('legoPieces', { // state for showing all kits
+    }).state('legoPieces', { // state for showing all pieces
         url: '/legoPieces',
         templateUrl: 'legoPieces.html',
         controller: 'LegoPieceListController'
-    }).state('categories', { // state for showing all kits
+    }).state('categories', { // state for showing all categories
         url: '/categories',
         templateUrl: 'categories.html',
         controller: 'CategoryListController'
