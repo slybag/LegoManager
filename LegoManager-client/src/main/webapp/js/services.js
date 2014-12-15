ea41ded4a6b523ed3284b37596237f9ef6fe36e6
xkonecn1 <xkonecn1@nymfe85.fi.muni.cz>
2014-12-15 15:59:23 +0100

    client shows error message

--- LegoManager-client/src/main/webapp/js/services.js
+++ LegoManager-client/src/main/webapp/js/services.js
@@ -1,15 +1,51 @@
 angular.module('legoApp.services', [])
 .factory('LegoKit', function($resource){
-  return $resource('http://localhost:8080/pa165/rest/legokits/:id',{id: '@id'}, {
-        update: {
-            method: 'PUT'
-        }
+    return $resource('http://localhost:8080/pa165/rest/legokits/:id',{id: '@id'}, {
+          update: {
+              method: 'PUT',
+            interceptor: {responseError : showAlert}
+          },
+          query: {
+              method: 'GET', isArray:true, 
+              interceptor: {responseError : showAlert}
+          },
+          get: {
+               method: 'GET', 
+              interceptor: {responseError : showAlert}         
+          },
+          save: {
+              method: 'POST', 
+              interceptor: {responseError : showAlert}  
+          },
+          remove: {
+              method: 'DELETE',
+            interceptor: {responseError : showAlert}  
+          }
     });
 })
 .factory('LegoPiece', function($resource){
     return $resource('http://localhost:8080/pa165/rest/legopieces/:id',{id: '@id'},{
         update: {
-            method: 'PUT'
-        }
+              method: 'PUT',
+            interceptor: {responseError : showAlert}
+          },
+          query: {
+              method: 'GET', isArray:true, 
+              interceptor: {responseError : showAlert}
+          },
+          get: {
+               method: 'GET', 
+              interceptor: {responseError : showAlert}         
+          },
+          save: {
+              method: 'POST', 
+              interceptor: {responseError : showAlert}  
+          },
+          remove: {
+              method: 'DELETE',
+            interceptor: {responseError : showAlert}  
+          }
     });    
 });
+
+function showAlert(){alert("Cannot connect to server, please try again later");}
